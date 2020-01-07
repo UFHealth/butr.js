402c5f0a8b82756db13787401464927b028c80e7
Bill Columbia <bcolumbia@ufl.edu>
2020-01-07 16:10:31 -0500

    add module api proposal, add example with simple web server for testing

--- src/butr.js
+++ src/butr.js
@@ -1,5 +1,17 @@
+import To from './to'
+import Marker from './marker'
+import AutoAnchors from './autoAnchors'
+import AutoSidebar from './autoSidebar'
+import StickyNav from './stickyNav'
+
 const init = (options) => {
   console.log(options)
+
+  if (options.to) To.init()
+  if (options.marker) Marker.init()
+  if (options.autoAnchors) AutoAnchors.init()
+  if (options.autoSidebar) AutoSidebar.init()
+  if (options.stickyNav) StickyNav.init()
 }
 
 window.Butr = {
