eb682d14dedd6848a5e4eeff8c4b7667db8617e2
Bill Columbia <bcolumbia@ufl.edu>
2021-03-29 10:38:07 -0400

    drop uglify, add terser, bump all deps that need it, test rebuild

--- build-system/config.webpack.js
+++ build-system/config.webpack.js
@@ -1,42 +1,50 @@
-const path = require('path')
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
-const isDev = process.env.NODE_ENV === 'development'
-const mode = isDev ? 'development' : 'production'
-const devtool = isDev ? 'eval' : false
+const isDev = process.env.NODE_ENV === "development";
+const mode = isDev ? "development" : "production";
+const devtool = isDev ? "eval" : false;
 
 module.exports = {
   mode,
   devtool,
   entry: {
-    Butr: './src/butr.js',
+    Butr: "./src/butr.js",
   },
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[name].js',
-    library: '[name]',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    umdNamedDefine: true
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].js",
+    library: "[name]",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    umdNamedDefine: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
-              ['@babel/preset-env', {
-                modules: false
-              }],
-            ]
-          }
-        }
-      }
-    ]
+              [
+                "@babel/preset-env",
+                {
+                  modules: false,
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.json']
-  }
-}
+    extensions: [".js", ".json"],
+  },
+  optimization: {
+    minimize: !isDev,
+    minimizer: isDev ? false : [new TerserPlugin()],
+  },
+};
